1cab6a514a2d74bc19837a4a96ead97a278e62da
wisnieskii <106274579+wisnieskii@users.noreply.github.com>
2022-07-05 14:08:20 -0400

    quase pronto

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-document.querySelector(".hamburguer").addEventListener("click", () =>
+document
+  .querySelector(".hamburguer")
+  .addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
-);
+  );
+
+document.querySelector("#qtde").addEventListener("change", atualizarPreco);
+
+function atualizarPreco() {
+  const qtde = document.querySelector("#qtde").Value;
+  let preco = qtde * 100;
+  document.querySelector("#preco").innerHTML = `R$ ${preco}`;
+}
